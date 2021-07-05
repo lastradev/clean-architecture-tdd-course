5566b75f650c7d5bf153185a51043b64f758b7f8
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-07-05 14:26:10 -0500

    Implement network info

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 
 import '../../../../core/error/exception.dart';
 import '../../../../core/error/failures.dart';
-import '../../../../core/platform/network_info.dart';
+import '../../../../core/network/network_info.dart';
 import '../../domain/entities/number_trivia.dart';
 import '../../domain/repositories/number_trivia_repository.dart';
 import '../datasources/number_trivia_local_data_source.dart';
---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,31 @@
+import 'package:clean_architecture_tdd_course/core/network/network_info.dart';
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  late NetworkInfoImpl networkInfo;
+  late MockDataConnectionChecker mockDataConnectionChecker;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to DataConnectionChecker.hasConnection',
+        () async {
+      // arrange
+      final tHasConnectionFuture = Future.value(true);
+      when(() => mockDataConnectionChecker.hasConnection)
+          .thenAnswer((_) => tHasConnectionFuture);
+      // act
+      final result = networkInfo.isConnected;
+      // assert
+      verify(() => mockDataConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
