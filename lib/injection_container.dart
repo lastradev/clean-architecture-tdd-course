16cde2296860975e30815fe0a73e6bf7a121d905
Oscar Lastra <56526315+lastra-dev@users.noreply.github.com>
2021-07-06 22:39:09 -0500

    Inject dependencies into main

---
+++ lib/injection_container.dart
@@ -0,0 +1,54 @@
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/network/network_info.dart';
+import 'core/util/input_converter.dart';
+import 'features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+// Service Locator
+final sl = GetIt.instance;
+
+void init() {
+  //! Features - Number Trivia
+  sl.registerFactory(() => NumberTriviaBloc(
+        concrete: sl(),
+        random: sl(),
+        inputConverter: sl(),
+      ));
+
+  // Use cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+  // Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+      () => NumberTriviaRepositoryImpl(
+            remoteDataSource: sl(),
+            localDataSource: sl(),
+            networkInfo: sl(),
+          ));
+
+  // Data sources
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(
+      () => NumberTriviaRemoteDataSourceImpl(client: sl()));
+
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(
+      () => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()));
+
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  //! External
+  sl.registerLazySingleton(() async => SharedPreferences.getInstance());
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'injection_container.dart' as di;
+
+void main() {
+  di.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
